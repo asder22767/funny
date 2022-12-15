1e2b7f9b97dbb2fef7f0e8fd45a9cdc7a10290d2
asder22767 <abrorbek22767@gmail.com>
2022-12-15 18:29:57 +0500

    something funny

---
+++ js/main.js
@@ -0,0 +1,23 @@
+var elBurger = document.querySelector(".something--tricky");
+var elHeader = document.querySelector(".lin");
+
+
+elBurger.addEventListener("click", function() {
+    elHeader.classList.toggle("just-joke")
+})
+
+var elBurgerr = document.querySelector(".something--trickyy");
+var elHeaderr = document.querySelector(".linn");
+
+
+elBurgerr.addEventListener("click", function() {
+    elHeaderr.classList.toggle("just-jokee")
+})
+
+var elBurgerrr = document.querySelector(".something--trickyyy");
+var elHeaderrr = document.querySelector(".linnn");
+
+
+elBurgerrr.addEventListener("click", function() {
+    elHeaderrr.classList.toggle("just-jokeee")
+})
